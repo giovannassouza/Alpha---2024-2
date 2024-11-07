6462980f4978c7758636f428b15a0c1ff915b57c
ArtMeTT1 <craveiroarthur@gmail.com>
2024-11-07 20:07:48 -0300

    initialize and create the database

--- back/website/__init__.py
+++ back/website/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 # from flask_login import LoginManager 
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -10,16 +9,27 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'OUR-PLATINA' #da pra mudar tbm nao é definitivo e tbm não é tãaao importante assim
-
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
     from .views import views
     from .auth import auth
     
+    # Initialize the database with the app
+    db.init_app(app)
+
+    # Imports classes from models
+    from .models import User, CursosEmProgresso, Curso, Questionario, Questao, Ementa, Aula, VideoAula, AcervoDeQuestoes, RespostaAoQuestionario
+    
+    # creates database
+    create_database(app)
+
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     return app
 
 
-# def create_database(app):
-#     if not path.exists('website/' + DB_NAME):
-#         db.create_all(app=app)
-#         print('Created Database!')
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+            print('Created Database!')
+

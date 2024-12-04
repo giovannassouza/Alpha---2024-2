daa52d195223146c5a8aa0149033e89d77791b24
Luis Menezes <luislipe4@gmail.com>
2024-12-04 16:35:15 -0300

    Added the certificate_api to create a pdf based on data from user and class

--- back/website/certificate.py
+++ back/website/certificate.py
@@ -1,16 +1,19 @@
 from flask import Blueprint, Flask, render_template
 from flask_login import login_required, current_user
+from .certificate_api import gerar_certificado_pdf
 from .models import *
 from website.models import *
 
 certificate = Blueprint('certificate', __name__)
 
 @certificate.route('/class/certificate', methods=['GET'])
-@login_required
+# @login_required
 def get_certificate():
     try:
-        questionarioFeito = db.session.execute(db.select(CursosEmProgresso).filter_by(user_id = current_user.get_id())).scalar_one()
+        cursosFeitos = db.session.execute(db.select(CursosEmProgresso).filter_by(user_id = current_user.get_id())).scalar_one()
         user_data = db.session.execute(db.select(User).filter_by(id=current_user.get_id()))
+        gerar_certificado_pdf(user_data.full_name, cursosFeitos.name, cursosFeitos.horas_estimado)
+        return "<p>Certificado Gerado!</p>"
 
     except Exception as e:
         # e holds description of the error

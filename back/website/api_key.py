abe5d410baf85d6acd5105576d4f6ec741e130af
Heitor Reis <tobiel.reis@gmail.com>
2025-01-31 13:08:01 -0300

    api_key is back!

---
+++ back/website/api_key.py
@@ -0,0 +1,29 @@
+# All APIs and Frameworks keys
+
+# App setup
+APP_SECRET = 'OUR-PLATINA'
+
+# DB setup
+DB_NAME = "sys"
+USER = 'root'
+PASSWORD = 'carecas2'
+HOST = 'localhost'
+PORT = 3306
+
+# MercadoPago key
+mercadoPago_key = "APP_USR-399152317369467-111409-e7711ec50d89b14605bc6f64ee5164e1-2095118211"
+
+# CORS setup
+# Lista de URLs do front-end permitidas
+FRONT_END_URLS = [
+    "http://127.0.0.1:5000/auth-test",
+    "http://frontend-dominio-exemplo2.com",
+    "http://frontend-dominio-exemplo3.com"
+]
+
+# TrapMail setup
+MAILTRAP_USERNAME = 'api'
+MAILTRAP_PASSWORD = 'd912a7b6f1e17fad33c403cee6f04c5d'
+MAILTRAP_PORT = 587
+MAILTRAP_HOST = 'live.smtp.mailtrap.io'
+MAILTRAP_API_KEY = 'd912a7b6f1e17fad33c403cee6f04c5d'

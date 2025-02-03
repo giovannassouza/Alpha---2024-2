80508652ff2a5ffe3560a2d4724ca07dfb5b8e73
Giovanna Souza <giovanna1432@hotmail.com>
2025-02-03 11:09:30 -0300

    Update csrf_.py

--- back/website/csrf_.py
+++ back/website/csrf_.py
@@ -6,4 +6,10 @@ csrf_ = Blueprint('csrf', __name__)
 @csrf_.route('/csrf-token', methods=['GET'])
 def get_csrf_token():
     csrf_token = generate_csrf()
-    return jsonify({'csrf_token': csrf_token})
+    response = jsonify({'csrf_token': csrf_token})
+    
+    # Adiciona os cabeçalhos CORS necessários
+    response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
+    response.headers.add("Access-Control-Allow-Credentials", "true")
+    
+    return response
